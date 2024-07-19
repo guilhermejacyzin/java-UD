bfd3f9a60449dfc2818fe47078239d0e61ad29b8
Guilherme Jacyzin <jacyzin@gmail.com>
2024-07-19 00:29:29 -0300

    Atualizando SEC05

--- S5S/src/Au33ExpressoesComparativas.java
+++ S5S/src/Au33ExpressoesComparativas.java
@@ -4,6 +4,8 @@ public class Au33ExpressoesComparativas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Hello");
+
 
 
 
