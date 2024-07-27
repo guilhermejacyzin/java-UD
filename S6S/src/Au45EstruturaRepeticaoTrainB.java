eefeae5d0ced56a098db0dc93a2414cf71e8c517
Guilherme Jacyzin <jacyzin@gmail.com>
2024-07-26 23:52:14 -0300

    Add files SEC06

--- S6S/src/Au45EstruturaRepeticaoTrainB.java
+++ S6S/src/Au45EstruturaRepeticaoTrainB.java
@@ -5,6 +5,15 @@ public class Au45EstruturaRepeticaoTrainB {
 
         Scanner input = new Scanner(System.in);
 
+        int x = input.nextInt();
 
+        int soma = 0;
+
+        while (x != 0) {
+            soma += x;
+            x = input.nextInt();
+        }
+
+        System.out.println(soma);
     }
 }
