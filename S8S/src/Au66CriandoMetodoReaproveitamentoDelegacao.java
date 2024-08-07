9afa75bad70aba779cb58163820f49709df6ea96
Guilherme Jacyzin <jacyzin@gmail.com>
2024-08-07 00:32:04 -0300

    SEC08 Add files

---
+++ S8S/src/Au66CriandoMetodoReaproveitamentoDelegacao.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Au66CriandoMetodoReaproveitamentoDelegacao {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+
+
+        input.close();
+    }
+}
