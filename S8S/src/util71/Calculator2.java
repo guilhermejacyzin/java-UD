63d68aa0da1c102a287136496dca48603ea4daf9
Guilherme Jacyzin <jacyzin@gmail.com>
2024-09-03 17:21:11 -0300

    ADD SEC08 file

---
+++ S8S/src/util71/Calculator2.java
@@ -0,0 +1,14 @@
+package util71;
+
+public class Calculator2 {
+
+    public static double PI = 3.14159;
+
+    public static double circunferencia(double radius) {
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3.0;
+    }
+}
