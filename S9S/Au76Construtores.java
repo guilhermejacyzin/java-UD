6aa1e20c840e5bb73228d3c637df8989a0ddfa98
Guilherme Jacyzin <jacyzin@gmail.com>
2024-09-10 16:39:00 -0300

    ADD SEC09 file

---
+++ S9S/Au76Construtores.java
@@ -0,0 +1,55 @@
+package S9S;
+
+import S9S.entities76.Au76Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Au76Construtores {
+    public static void main(String[] args) {
+
+    /*
+    Palavra This, Sobrecarga, Encapsulamento
+     */
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+//        Au76Product product = new Au76Product();
+
+        System.out.print("Digite o nome do produto: ");
+        var produto = input.nextLine();
+
+        System.out.print("Digite o preço do produto: ");
+        var preco = input.nextDouble();
+
+        System.out.print("Digite o quantidade do produto: ");
+        var quantidade = input.nextInt();
+
+        Au76Product product = new Au76Product(produto, preco, quantidade);
+
+        System.out.println("Dados do produto: " + product.toString());
+
+        System.out.println();
+
+        System.out.print("Digite o número de produtos adicionados no estoque: ");
+        quantidade = input.nextInt();
+        product.addProducts(quantidade);
+
+        System.out.println();
+
+        System.out.println("Dados do produto atualizados: " + product.toString());
+        System.out.println();
+
+        System.out.print("Digite o número de produtos que quer remover: ");
+        quantidade = input.nextInt();
+        product.removeProducts(quantidade);
+
+
+        System.out.println();
+        System.out.println("Dados do produto atualizados: " + product.toString());
+
+        input.close();
+    }
+
+}
---
+++ S9S/entities76/Au76Product.java
@@ -0,0 +1,32 @@
+package S9S.entities76;
+
+public class Au76Product {
+
+    public String produto;
+    public double preco;
+    public int quantidade;
+
+    public Au76Product(String produto, double preco, int quantidade) {
+        this.produto = produto;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public double totalValueInStock() {
+        return preco * quantidade;
+    }
+
+    public void addProducts(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removeProducts(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+
+    public String toString() {
+        return produto + ", $ " + String.format("%.2f", preco) + ", " + quantidade + " unidades, Total: " + String.format("%.2f",totalValueInStock());
+
+    }
+
+}
