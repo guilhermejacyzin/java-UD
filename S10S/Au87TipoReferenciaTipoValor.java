4fce6aa144c9cb5944cb80ac72560c01a3c079ab
Guilherme Jacyzin <jacyzin@gmail.com>
2024-09-19 23:59:52 -0300

    ADD SEC10 file

---
+++ S10S/Au87TipoReferenciaTipoValor.java
@@ -0,0 +1,7 @@
+package S10S;
+
+public class Au87TipoReferenciaTipoValor {
+    public static void main(String[] args) {
+        //Nota
+    }
+}
---
+++ S10S/Au89VetoresParte1.java
@@ -0,0 +1,16 @@
+package S10S;
+
+public class Au89VetoresParte1 {
+    public static void main(String[] args) {
+        /*
+        Revisão de conceito de vetor
+        Declaração e instanciação
+        Manipulação de vetor de elementos tipo valor (tipo primitivo)
+        Manipulação de vetor de elementos tipo referência (classe)
+        Acesso aos elementos
+        Propriedade length
+         */
+
+
+    }
+}
