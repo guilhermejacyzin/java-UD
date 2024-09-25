c90f8f301a13a9d1aea4f3de4a8ac0618729f80d
Guilherme Jacyzin <jacyzin@gmail.com>
2024-09-24 23:47:49 -0300

    ADD SEC10 file

---
+++ S10S/Au90VetoresParte2.java
@@ -0,0 +1,15 @@
+package S10S;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Au90VetoresParte2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        input.close();
+    }
+}
---
+++ S10S/Au90entities/Au90Product.java
@@ -0,0 +1,7 @@
+package S10S.Au90entities;
+
+public class Au90Product {
+
+    private String name;
+    private int price;
+}
