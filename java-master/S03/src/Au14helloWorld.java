65dfb2b1811f06e444fe2c618c44305fcdfddc14
Guilherme Jacyzin <jacyzin@gmail.com>
2024-11-20 01:08:12 -0300

    New

---
+++ java-master/S03/src/Au14helloWorld.java
@@ -0,0 +1,17 @@
+public class Au14helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+
+
+
+        short shortValue = 20;
+
+        int intValue = 50;
+
+        long longTotal = 50800;
+
+        System.out.println(shortValue);
+        System.out.println(intValue);
+        System.out.println(longTotal);
+    }
+}
