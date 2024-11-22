66bf8b69efaa65f8ef83af69b23f1c2ab59371fe
Guilherme Jacyzin <jacyzin@gmail.com>
2024-11-22 01:49:15 -0300

    SEC03

---
+++ java-master/S03/src/Au24PrimitiveTypesRecapStringDataType.java
@@ -0,0 +1,22 @@
+public class Au24PrimitiveTypesRecapStringDataType {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tipos Primitivos de Dados");
+
+        String myString = "This is a string";
+
+        System.out.println(myString);
+
+        System.out.println("myString is equal to " + myString);
+
+        String myString2 = myString + " This is a string";
+
+        System.out.println(myString2);
+
+        String myString3 = "I wish I had \u00241,000,000.00";
+
+        System.out.println(myString3);
+
+    }
+}
