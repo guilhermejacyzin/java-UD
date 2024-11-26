5364d6b28e03ba45b91fe489dd8d5d9474aca6b3
Guilherme Jacyzin <jacyzin@gmail.com>
2024-11-26 01:39:16 -0300

    SEC04

---
+++ java-master/S04/src/Au34IfThenStatement.java
@@ -0,0 +1,35 @@
+public class Au34IfThenStatement {
+
+    public static void main(String[] args) {
+
+        System.out.println("O if-then executa o código somente se determinada seção de código" +
+                " somente se um determinado teste for avaliado como verdadeiro");
+
+
+        System.out.println("Lógica condicional");
+
+        System.out.println("Olá, Gui");
+
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("Não é um alienígena!");
+            System.out.println("E eu tenho medo de aliens");
+
+
+        /*
+        if (expressão) {
+            instrução
+
+        }
+
+         */
+
+        System.out.println();
+        boolean isAlien2 = false;
+        if(isAlien2 == false) {
+            System.out.println("Não é um alienígena!");
+            System.out.println("E eu tenho medo de aliens");
+        }
+
+    }
+}
