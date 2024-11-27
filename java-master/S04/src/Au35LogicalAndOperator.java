112d592bf99b7cb5e040d8ece91f69778bdbeaf8
Guilherme Jacyzin <jacyzin@gmail.com>
2024-11-27 00:02:07 -0300

    SEC04

---
+++ java-master/S04/src/Au35LogicalAndOperator.java
@@ -0,0 +1,29 @@
+public class Au35LogicalAndOperator {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        boolean isAlien2 = false;
+        if(isAlien2 == false) {
+            System.out.println("Não é um alienígena!");
+            System.out.println("E eu tenho medo de aliens");
+
+        }
+
+        System.out.println();
+
+        int topScore = 80;
+        if (topScore <= 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("topScore é Maior que secondTopScore e menor que 100");
+        }
+
+
+
+    }
+
+}
