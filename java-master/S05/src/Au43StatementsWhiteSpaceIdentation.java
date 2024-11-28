34653c935105813492b442c5eef4b77d804a4c9d
Guilherme Jacyzin <jacyzin@gmail.com>
2024-11-28 00:13:07 -0300

    SEC05

---
+++ java-master/S05/src/Au43StatementsWhiteSpaceIdentation.java
@@ -0,0 +1,25 @@
+public class Au43StatementsWhiteSpaceIdentation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int myVariable = 50;
+
+        System.out.println(myVariable);
+
+        myVariable++;
+        System.out.println(myVariable);
+        myVariable--;
+        System.out.println(myVariable);
+
+        System.out.println("Este é" +
+                " um outro" +
+                " teste de escrita");
+
+        int anotherVariable = 50; myVariable--;
+        System.out.println("myVariable = " + myVariable);
+
+    }
+
+}
