25408eea3b04bcafad25508b43c2a39a35511439
Guilherme Jacyzin <jacyzin@gmail.com>
2024-12-02 23:39:05 -0300

    SEC05

---
+++ java-master/S05/src/Au48BVersion.java
@@ -0,0 +1,15 @@
+public class Au48BVersion {
+
+    public static void main(String[] args) {
+
+        System.out.println("Args");
+
+        boolean carro = true;
+
+        if (carro) {
+            System.out.println("É verdade");
+        }
+
+    }
+
+}
