a0bdbee78cc1e276d2c41ecb2bf144ad82ef4f5e
Guilherme Jacyzin <jacyzin@gmail.com>
2024-12-03 01:40:08 -0300

    SEC05

---
+++ java-master/S05/src/Au49MethodChallengeB.java
@@ -0,0 +1,29 @@
+public class Au49MethodChallengeB {
+
+    public static void main(String[] args) {
+
+        int preferenciaCarro = faixaPreco(2);
+        tipoCarro("Corsa", preferenciaCarro);
+    }
+
+    public static void tipoCarro(String nomeCarro, int precoCarro) {
+        System.out.println("Carro: " + nomeCarro + " com preço de " + precoCarro);
+    }
+
+    public static int faixaPreco (int preco) {
+
+        if (preco == 1) {
+            return 1000;
+        }
+        else if (preco == 2) {
+            return 2000;
+        } else if (preco == 3) {
+            return 3000;
+        } else {
+            return 4000;
+        }
+    }
+
+}
+
+
