55e653484e05faccd8850b79f7d20c73d5fb7390
Guilherme Jacyzin <jacyzin@gmail.com>
2024-12-05 01:44:00 -0300

    SEC06

---
+++ java-master/S06/src/AuEX.java
@@ -0,0 +1,12 @@
+public class AuEX {
+    public static void main(String[] args) {
+
+
+        int nota = 10;
+
+        boolean minhaNota = nota < 5 ? true : false;
+        System.out.println(minhaNota);
+
+
+    }
+}
