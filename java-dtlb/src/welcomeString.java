6f37f3685ab6dc38581e1420714a78f202555352
Guilherme Jacyzin <jacyzin@gmail.com>
2024-12-09 23:39:28 -0300

    SEC06

---
+++ java-dtlb/src/welcomeString.java
@@ -0,0 +1,10 @@
+public class welcomeString {
+
+    public static void main(String[] args) {
+
+        String welcome = "Seja bem vindo, Guilherme";
+
+        System.out.println(welcome);
+
+    }
+}
